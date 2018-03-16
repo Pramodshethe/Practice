c655eb8462447a95e238e445c15ceaaafd55390e
Pramodkumar <pramods35894@gmail.com>
2018-03-16 20:09:00 +0530

    IJavaP

--- IJavaPgm/src/com/pramod/example/Pattern1.java
+++ IJavaPgm/src/com/pramod/example/Pattern1.java
@@ -4,7 +4,7 @@ public class Pattern1 {
     public static void main(String[] args) {
         int n = 5;
         for (int i = 0; i < n; i++){
-            for (int j = n-1; j<=i ; j++){
+            for (int j = 0; j<=i ; j++){
                 System.out.print("*");
             }
             System.out.println("");
